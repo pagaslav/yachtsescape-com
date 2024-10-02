d23ca33760a0cff5dc3815b5f9480ab91b8c56cf
Artem Bryzh <aab@AMBM2.local>
2024-10-02 18:02:03 +0100

    chore(settings): add heroku to allowed hosts and secret key

--- yachtsescape/settings.py
+++ yachtsescape/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +27,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%d(%$-cgca=#8be_lo$77+86)!xm*zhynhce0qttrr2!&qtz78'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yachtsescape-b724d0be9908.herokuapp.com']
 
 
 # Application definition
