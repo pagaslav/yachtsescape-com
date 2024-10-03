dd468cdfaaf5095bf587f33b05f59e63d8e2a0eb
Artem Bryzh <aab@AMBM2.local>
2024-10-03 19:35:39 +0100

    Change settings for AWS service with new bucket

--- yachtsescape/settings.py
+++ yachtsescape/settings.py
@@ -154,7 +154,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'yachtsescape-bucket'
+    AWS_STORAGE_BUCKET_NAME = 'yachtsescape'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
