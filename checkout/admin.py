151694f0ef55f59eec15a3181eaee25b484855e3
Artem Bryzh <aab@AMBM2.local>
2024-10-13 16:50:38 +0100

    feat(admin): configure admin for Order model
    Added custom configuration for the Order model in the Django admin panel. Included readonly fields
    (order_number, date, total_cost, stripe_pid) and configured fields for order details page and list
    display in the admin.  This setup provides a convenient interface for managing and reviewing orders.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+# checkout/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    # Fields to display in readonly mode
+    readonly_fields = ('order_number', 'date', 'total_cost', 'stripe_pid')
+    
+    # Fields to display in the order form within the admin panel
+    fields = ('order_number', 'user_profile', 'booking', 'date', 
+              'full_name', 'email', 'phone_number', 'country', 
+              'postcode', 'town_or_city', 'street_address1', 
+              'street_address2', 'county', 'total_cost', 'stripe_pid', 
+              'status')
+    
+    # Fields to display in the list of orders
+    list_display = ('order_number', 'date', 'full_name', 'total_cost', 'status')
+    
+    # Ordering of the list display
+    ordering = ('-date',)
+
+# Registering the Order model with the custom admin options
+admin.site.register(Order, OrderAdmin)
