fb7f8f437fcb01d467678ff4ebc6ea86c9300560
Artem Bryzh <aab@AMBM2.local>
2024-10-16 00:27:55 +0100

    feat(admin): customize Profiles section in Django admin panel
    Enhanced the appearance and layout of the Profiles section in the admin panel for improved usability
    and clarity.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+# profiles/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'phone_number', 'street_address1', 'street_address2', 
+                    'town_city', 'county_state', 'postal_code', 'country')
+    search_fields = ('user__username', 'phone_number', 'street_address1', 'town_city')
+    list_filter = ('country', 'county_state')
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
