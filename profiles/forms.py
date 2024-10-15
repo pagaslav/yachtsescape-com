2b506de1ca3229c61c46dead158ae57c33ac8449
Artem Bryzh <aab@AMBM2.local>
2024-10-16 00:29:39 +0100

    refactor(forms): update UserProfileForm fields in profiles app
    Replaced default address and contact fields with new standardized fields in UserProfileForm to align
    with updated UserProfile model structure.

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,11 +1,10 @@
 # profiles/forms.py
+
 from django import forms
 from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['default_phone_number', 'default_country', 
-                  'default_postcode', 'default_town_or_city', 
-                  'default_street_address1', 'default_street_address2', 
-                  'default_county']
+        fields = ['phone_number', 'street_address1', 'street_address2', 
+                  'town_city', 'county_state', 'postal_code', 'country']
