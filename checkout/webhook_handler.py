3891f7ee8a68b4a8508f9e1832f0fc427f316c76
Artem Bryzh <aab@AMBM2.local>
2024-10-17 14:56:20 +0100

    fix(webhook handler): comment send confirmation email function

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,26 +21,26 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def _send_confirmation_email(self, booking):
-        """Sends a confirmation email to the user after a booking is successfully processed"""
-        cust_email = booking.email
-        subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'booking': booking})
-        body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'booking': booking, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    # def _send_confirmation_email(self, booking):
+    #     """Sends a confirmation email to the user after a booking is successfully processed"""
+    #     cust_email = booking.email
+    #     subject = render_to_string(
+    #         'checkout/confirmation_emails/confirmation_email_subject.txt',
+    #         {'booking': booking})
+    #     body = render_to_string(
+    #         'checkout/confirmation_emails/confirmation_email_body.txt',
+    #         {'booking': booking, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-        try:
-            send_mail(
-                subject,
-                body,
-                settings.DEFAULT_FROM_EMAIL,
-                [cust_email]
-            )
-            logger.info(f"Confirmation email sent to {cust_email}")
-        except Exception as e:
-            logger.error(f"Error sending email: {e}")
+    #     try:
+    #         send_mail(
+    #             subject,
+    #             body,
+    #             settings.DEFAULT_FROM_EMAIL,
+    #             [cust_email]
+    #         )
+    #         logger.info(f"Confirmation email sent to {cust_email}")
+    #     except Exception as e:
+    #         logger.error(f"Error sending email: {e}")
 
     def handle_event(self, event):
         """Handles a generic/unknown/unexpected webhook event from Stripe."""
@@ -51,23 +51,23 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """Handles the payment_intent.succeeded webhook from Stripe."""
-        # intent = event.data.object
+        intent = event.data.object
         session = event.data.object
-        # logger.info(f"Intent data: {json.dumps(intent, indent=2)}")
+        logger.info(f"Intent data: {json.dumps(intent, indent=2)}")
         logger.info(f"Intent data: {json.dumps(session, indent=2)}")
         
         # Retrieve the booking ID from metadata
         booking_id = getattr(session, 'client_reference_id', None)
-        # booking_id = intent.metadata.get('booking_id')
-        # logger.error(f"Metadata received: {intent.metadata}")
+        booking_id = intent.metadata.get('booking_id')
+        logger.error(f"Metadata received: {intent.metadata}")
         logger.error(f"Retrieved booking_id: {booking_id}")
 
         # Check if booking_id is None
-        # if booking_id is None:
-        #     logger.error("Booking ID is missing in the webhook metadata.")
-        #     return HttpResponse(
-        #         content=f'Webhook received: {event["type"]} | ERROR: Booking ID missing',
-        #         status=400)
+        if booking_id is None:
+            logger.error("Booking ID is missing in the webhook metadata.")
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | ERROR: Booking ID missing',
+                status=400)
 
         try:
             # Fetch the booking associated with this payment
@@ -77,7 +77,7 @@ class StripeWH_Handler:
             booking.confirm_booking()  # Update status to confirmed
             
             # Send confirmation email
-            self._send_confirmation_email(booking)
+            # self._send_confirmation_email(booking)
             
             logger.info(f"Booking {booking_id} confirmed and email sent.")
             return HttpResponse(
@@ -114,7 +114,7 @@ class StripeWH_Handler:
             # Fetch the booking associated with this payment
             booking = Booking.objects.get(id=booking_id)
             booking.confirm_booking()  # Update status to confirmed
-            self._send_confirmation_email(booking)
+            # self._send_confirmation_email(booking)
             logger.info(f"Booking {booking_id} confirmed and email sent.")
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Booking confirmed',
