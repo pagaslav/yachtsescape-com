1e312a2f2f639ce8ed031c590bc4e12697d32fc3
Artem Bryzh <aab@AMBM2.local>
2024-10-20 14:50:01 +0100

    feat(about): add comditional choise for aythenticated users

--- about/urls.py
+++ about/urls.py
@@ -4,7 +4,5 @@ from django.urls import path
 from .views import AboutView
 
 urlpatterns = [
-    path('', AboutView.as_view(), name='about'),  # Route for the About page
-    # path('review/<int:review_id>/helpful/', mark_helpful, name='mark_helpful'),
-    # path('review/<int:review_id>/report/', report_review, name='report_review'),
+    path('', AboutView.as_view(), name='about'),
 ]
